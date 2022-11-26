48f9ea460b5a00cf34fedd1263003c7c23dff6a7
DemouserNG <108471967+DemouserNG@users.noreply.github.com>
2022-11-26 12:46:24 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
 
+import throttle from "lodash.throttle";
+
+const ref = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    button: document.querySelector('.feedback-form button')
+}
+
+ref.form.addEventListener('input', throttle(onFormText, 500));
+
+ref.form.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+
+populateTextareaInput();
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+// для усіх полів форми
+
+ref.form.addEventListener('input', e => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+    // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+})
+
+function onFormText(evn) {
+    const message = evn.target.value;
+    console.log(message);
+
+    // localStorage.setItem(STORAGE_KEY, message);
+    
+}
+
+
+function onFormSubmit(evn) {
+    evn.preventDefault();
+    evn.currentTarget.reset();
+    // очищає форму після відправлення
+    localStorage.removeItem(STORAGE_KEY);
+    // очищає localStorage після відправлення
+
+
+}
+
+function populateTextareaInput() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (saveMessage) {
+        console.log(saveMessage);
+    }
+
+    ref.textarea.value = saveMessage;
+    ref.input.value = saveMessage;
+}
